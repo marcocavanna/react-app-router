5d4cfc4b4be4924a0ecb03a6a23dc42465df1fd9
Marco Cavanna <info@marcocavanna.com>
2020-05-22 19:00:36 +0200

    feat(applink): components to wrap links

---
+++ src/components/AppLink/AppLink.interfaces.ts
@@ -0,0 +1,28 @@
+import type { NavLinkProps } from 'react-router-dom';
+
+import type { RouteParams } from '../../context/app.router.context.interfaces';
+
+
+export interface AppLinkProps<T extends string = string> extends Omit<NavLinkProps
+  , 'to'> {
+  /** Render the link as NavLink */
+  asNavLink?: boolean;
+
+  /** Params to use to build the url */
+  params?: RouteParams;
+
+  /**
+   * By default, a AppLink will be rendered
+   * only if current user could reach the
+   * requested route.
+   * If a user has no auth, a link to a private
+   * route will no be rendered.
+   *
+   * Set this props to `true` if link must be
+   * rendered anyway
+   */
+  renderAnyway?: boolean;
+
+  /** Route name. Must be one of the defined route of AppRouter component */
+  to: T;
+}
